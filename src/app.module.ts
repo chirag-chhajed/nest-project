85aa4a373d11352780bc1cc0bf6da98cfd7917c7
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-09-09 00:15:19 +0530

    boilerplate code

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { EventsController } from './events.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Event } from './event.entity';
+
+@Module({
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: '127.0.0.1',
+            port: 3306,
+            username: 'root',
+            password: '94justchaT.',
+            database: 'events',
+            entities: [Event],
+            synchronize: true,
+        }),
+    ],
+    controllers: [AppController, EventsController],
+    providers: [AppService],
+})
+export class AppModule {}
